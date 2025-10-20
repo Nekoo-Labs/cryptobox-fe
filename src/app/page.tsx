8e4446c7cbf048134603ceff1240020a9b85f565
Farrely-F <farrely.firenza@outlook.com>
2025-10-20 23:35:08 +0700

    feat: add initial UnboxStepsSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { HeroSection, NftShowcaseSection } from "@/components";
+import {
+  HeroSection,
+  NftShowcaseSection,
+  UnboxStepsSection,
+} from "@/components";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <NftShowcaseSection />
+      <UnboxStepsSection />
     </main>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Navbar } from "./navbar";
 export { ConnectWalletButton } from "./connect-wallet-button";
 export { HeroSection } from "./hero-section";
 export { NftShowcaseSection } from "./nft-showcase-section";
+export { UnboxStepsSection } from "./unbox-steps-section";
